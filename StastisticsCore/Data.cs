242bb797ba1ff79cf1ae89cf2756abedd8005901
CTrigger <hexadecimal.exe@gmail.com>
2024-10-20 12:34:31 +0100

    Projects data base handlers

---
+++ StastisticsCore/Data.cs
@@ -0,0 +1,20 @@
+﻿namespace StastisticsCore
+{
+    public class Data
+    {
+        #region Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        #endregion
+
+        #region Ctor
+        public Data() { }
+        public Data(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        #endregion
+    }
+}
