28f7e294e98409634809568cf6b66262255a1dd7
CTrigger <hexadecimal.exe@gmail.com>
2024-10-20 13:25:28 +0100

    Renamed class

---
+++ StastisticsCore/MeanDataCore.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace StastisticsCore
+{
+    public class MeanDataCore : DataCore
+    {
+        public double WeightedAritmeticMean { get => Dataset.Sum(d => (d.Y * d.X) / SumOfFrequencies); }
+        public double WeightedGeometricMean { get => Dataset.Aggregate(0.0, (acc, d) => acc * System.Math.Pow(d.X, d.Y / SumOfFrequencies)); }
+        public double WeightedHarmonicMean { get => SumOfFrequencies / Dataset.Sum(d => d.Y / d.X); }
+
+
+
+    }
+}
