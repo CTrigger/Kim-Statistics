0eac1397f960d9e7a97631495a426ef738002eb7
CTrigger <hexadecimal.exe@gmail.com>
2024-10-20 13:25:43 +0100

    mean insertion

---
+++ StastisticsCore/DataCore.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StastisticsCore
+{
+    public class DataCore
+    {
+        #region Properties
+        public List<Data> Dataset { get; set; }
+        public double SumOfFrequencies { get => Dataset.Sum(d => d.X); }
+        #endregion
+
+        #region Ctor
+        public DataCore()
+        {
+            Dataset = new List<Data>();
+        }
+        #endregion
+
+        #region Methods
+        public void LoadData(List<Data> dataset)
+        {
+            Dataset = dataset;
+        }
+
+        #region Add
+        public void AddItem(double y)
+        {
+            double x = Dataset.LastOrDefault()?.X ?? 0;
+            x++;
+            Dataset.Add(new Data(x, y));
+        }
+        public void AddItem(double x, double y)
+        {
+            Dataset.Add(new Data(x, y));
+        }
+
+        #endregion
+
+        #region Remove
+        public int RemoveItem(double x)
+        {
+            return Dataset.RemoveAll(d => d.X == x);
+        }
+        public int RemoveItem(double x, double y)
+        {
+            return Dataset.RemoveAll(d => d.X == x && d.Y == y);
+        }
+        #endregion
+
+        #region Change
+        public void ChangeValue(double x, double y)
+        {
+            int index = Dataset.FindIndex(d => d.X == x);
+            if (index != -1)
+                throw new System.Exception($"Index not found: {x}");
+
+            Dataset[index].Y = y;
+        }
+        #endregion
+
+        #endregion
+    }
+}
